fae10aa77d26f8f1af667d1e6178c11586d789ea
goran <goran.novacic@gmail.com>
2020-11-30 18:48:18 +0100

    #roles_permissions_activations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,26 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Model::unguard();
+
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(ConnectRelationshipsSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        //$this->call('UsersTableSeeder');
+
+        Model::reguard();
     }
 }
